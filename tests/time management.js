b9b052df66f49bd575960f08fa7408b951d7b85a
Assaf06 <assafelal06@gmail.com>
2023-01-16 14:39:18 +0200

    all questions
    add scenarios

---
+++ tests/time management.js
@@ -0,0 +1,16 @@
+//Explain in bullets and % - 
+//what were your roles and responsibilities 
+//and how was your time divided between tasks in a project?
+
+// Roles and responsibilities: 
+// 1. Understand the given exercise    
+// 2. Create an high-level plan/tasks 
+// 3. Get familiar with the system (DataLoops)
+// 4. Build the test (+ answer all the other qeustions/tasks)
+// 5. Refactor 
+
+// Time divison: 
+// 15% get familiar with the tasks and the system
+// 50% build the automated test
+// 20% answer other questions 
+// 15% refactor 
